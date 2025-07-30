0845fa4a3dd7f123f4e52d20f2bd111e4621c050
sneha2835 <snehakamatam28@gmail.com>
2025-07-31 00:00:25 +0530

    Update exceptions.py: enhanced error handlers

--- backend/app/exceptions.py
+++ backend/app/exceptions.py
@@ -5,6 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class UnicornException(HTTPException):
     def __init__(self, status_code: int, detail: str):
         super().__init__(status_code=status_code, detail=detail)
