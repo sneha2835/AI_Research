19ddd81d1c6201b70d0e58758294c1c9eefb4650
sneha2835 <snehakamatam28@gmail.com>
2025-09-21 22:15:18 +0530

    {commit_message}

--- run.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-model_name = "distilgpt2"  # Your current model
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
-
-print("Model loaded successfully")
